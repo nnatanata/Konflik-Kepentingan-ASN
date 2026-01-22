ba6237a1c836a450ab8e06fc3aa7277e029053f7
nnatanata <shofirnata26@gmail.com>
2026-01-22 11:39:15 +0700

    membuat migrations

---
+++ database/migrations/2026_01_22_033029_create_pengguna_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pengguna', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();
+            $table->string('password');
+            $table->enum ('role', ['pegawai', 'verifikator', 'inspektorat']);
+
+            $table->string('pegawai_nip')->nullable();
+            $table->foreign('pegawai_nip')->references('nip')->on('pegawai');
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->string('created_by')->nullable();
+            $table->string('updated_by')->nullable();
+            $table->string('deleted_by')->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
